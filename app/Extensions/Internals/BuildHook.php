919fb7466ea2bd6902efa8bff03bf21a9cbf85da
Krystian Duma <git@krystian.duma.sh>
2023-12-16 17:20:00 +0100

    Make BuildHook repeatable

--- app/Extensions/Internals/BuildHook.php
+++ app/Extensions/Internals/BuildHook.php
@@ -4,7 +4,7 @@ namespace App\Extensions\Internals;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class BuildHook
 {
     public function __construct(
