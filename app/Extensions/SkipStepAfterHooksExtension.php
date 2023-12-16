05a7c48fb458cf588686a6a4e5812d7f3a9f346f
Krystian Duma <git@krystian.duma.sh>
2023-12-16 17:20:25 +0100

    Migrate from skip-final-cleanup to skip-step-after-hook extension

---
+++ app/Extensions/SkipStepAfterHooksExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Extensions;
+
+use App\Extensions\Internals\BuildExtension;
+use App\Extensions\Internals\BuildHook;
+use App\Extensions\Internals\BuildHooks;
+use App\Extensions\Internals\BuildState;
+use App\Extensions\Internals\BuildStateInterface;
+use App\Extensions\Internals\ExtensionRunner;
+use App\Extensions\Internals\PluginConfig;
+use Illuminate\Support\Collection;
+
+#[BuildExtension(name: 'Skip Step After Hook', key: 'skip-step-after-hook')]
+class SkipStepAfterHooksExtension
+{
+    #[BuildHook(BuildHooks::BEFORE_CREATE_TEMP_DIRECTORY)]
+    #[BuildHook(BuildHooks::BEFORE_DOWNLOAD_FROM_S3)]
+    #[BuildHook(BuildHooks::BEFORE_BUILD_SATIS_REPOSITORY)]
+    #[BuildHook(BuildHooks::BEFORE_UPLOAD_TO_S3)]
+    #[BuildHook(BuildHooks::BEFORE_REMOVE_MISSING_FILES_FROM_S3)]
+    #[BuildHook(BuildHooks::BEFORE_FINAL_CLEAR_TEMP_DIRECTORY)]
+    #[BuildHook(BuildHooks::BEFORE_INITIAL_CLEAR_TEMP_DIRECTORY)]
+    public function hook(BuildHooks $hook, PluginConfig $config): bool
+    {
+        return ! $config->get('skip', collect())->contains($hook->name);
+    }
+}
