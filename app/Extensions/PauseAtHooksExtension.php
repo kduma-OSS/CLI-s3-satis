f5d605715306ab65917da81055f0c5d5c78ab603
Krystian Duma <git@krystian.duma.sh>
2023-12-16 17:27:21 +0100

    pause-at-hook extension

---
+++ app/Extensions/PauseAtHooksExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Extensions;
+
+use App\Extensions\Internals\BuildExtension;
+use App\Extensions\Internals\BuildHook;
+use App\Extensions\Internals\BuildHooks;
+use App\Extensions\Internals\BuildState;
+use App\Extensions\Internals\BuildStateInterface;
+use App\Extensions\Internals\ExtensionRunner;
+use App\Extensions\Internals\PluginConfig;
+use Illuminate\Support\Collection;
+use LaravelZero\Framework\Commands\Command;
+
+#[BuildExtension(name: 'Pause At Hook', key: 'pause-at-hook')]
+class PauseAtHooksExtension
+{
+    #[BuildHook(BuildHooks::BEFORE_INITIAL_CLEAR_TEMP_DIRECTORY)]
+    #[BuildHook(BuildHooks::AFTER_INITIAL_CLEAR_TEMP_DIRECTORY)]
+    #[BuildHook(BuildHooks::BEFORE_CREATE_TEMP_DIRECTORY)]
+    #[BuildHook(BuildHooks::AFTER_CREATE_TEMP_DIRECTORY)]
+    #[BuildHook(BuildHooks::BEFORE_DOWNLOAD_FROM_S3)]
+    #[BuildHook(BuildHooks::AFTER_DOWNLOAD_FROM_S3)]
+    #[BuildHook(BuildHooks::BEFORE_BUILD_SATIS_REPOSITORY)]
+    #[BuildHook(BuildHooks::AFTER_BUILD_SATIS_REPOSITORY)]
+    #[BuildHook(BuildHooks::BEFORE_UPLOAD_TO_S3)]
+    #[BuildHook(BuildHooks::AFTER_UPLOAD_TO_S3)]
+    #[BuildHook(BuildHooks::BEFORE_REMOVE_MISSING_FILES_FROM_S3)]
+    #[BuildHook(BuildHooks::AFTER_REMOVE_MISSING_FILES_FROM_S3)]
+    #[BuildHook(BuildHooks::BEFORE_FINAL_CLEAR_TEMP_DIRECTORY)]
+    #[BuildHook(BuildHooks::AFTER_FINAL_CLEAR_TEMP_DIRECTORY)]
+    public function hook(BuildHooks $hook, PluginConfig $config, Command $command): void
+    {
+        if($config->get('pause', collect())->contains($hook->name)) {
+            $command->warn("Paused at hook {$hook->name}.");
+            $command->ask("Press enter to continue...");
+        }
+    }
+}
